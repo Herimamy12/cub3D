f4c7048bb6a5a4ee627cf7acf60197f6ea69e87b
nherimam <nherimam@nherimam42.home>
2024-11-07 23:25:58 +0300

    removed readme

--- src/fill_screen2D.c
+++ src/fill_screen2D.c
@@ -29,7 +29,6 @@ void	fill_brick_wall2d(t_data *data, int height, int width)
 		{
 			mlx_pixel_put (data->win->mlx_ptr, data->win->mlx_win,
 				initw, inith, WHITE);
-			// forb_addback (&(data->forb), new_forb (initw, inith));
 			initw++;
 		}
 		inith++;
@@ -50,7 +49,7 @@ void	fill_screen2d(t_data *data)
 			if (data->map->map[height][width] == '1')
 			{
 				forb_addback (&(data->forb), new_forb (width, height));
-				fill_brick_wall2d (data, height, width);
+				// fill_brick_wall2d (data, height, width);
 			}
 			width++;
 		}
